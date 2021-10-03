66df0ca743d0de304bcda00091af0ce14690570f
reggie9594 <89363720+reggie9594@users.noreply.github.com>
2021-10-03 16:36:37 +1100

    final time sheet

--- classes/timesheet.rb
+++ classes/timesheet.rb
@@ -1,10 +1,21 @@
-require 'tty-prompt'
-
 class Timesheet
 	attr_reader :days
 
 	def initialize(payrate, days)
 		@payrate = payrate
 		@days = days
+	end
+
+	def display_timesheet(staff, company)
+		puts "Payrate #{@payrate.type}"
+		puts "They worked on"
+		@days.each {|day| puts " *#{day}"}
+		puts "#{staff.full_name} worked #{@days.length} days"
+		puts "Total Pay : $#{'%.2f' % payrate_amount}"
+	end
+
+
+	def payrate_amount
+		return @payrate.rate.to_f * @days.length
 	end
 end
