8eec28c9f9a382c463f004e02736f075c3b0dcd5
reggie9594 <89363720+reggie9594@users.noreply.github.com>
2021-10-03 16:37:28 +1100

    final staff

--- classes/staff.rb
+++ classes/staff.rb
@@ -1,15 +1,16 @@
-require 'tty-prompt'
 class Staff
-     attr_accessor :full_name, :dob, :payrate
+     attr_accessor :full_name, :dob, :payrate, :timesheet
      def initialize(full_name, dob, payrate)
           @full_name = full_name
           @dob = dob
           @payrate = payrate
+          @timesheet = nil
      end
 
      def display_details()
         puts "Staff Name: #@full_name"
         puts "Staff DoB: #@dob"
-        puts "Staff Payrate: #@payrate"
+        # puts "Staff Payrate: #@payrate"
+        # puts "Staff Rate: #@rate"
      end
 end
